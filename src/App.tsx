92362eaeee89ad7cd675a8f97db25a47794bd53f
farbod-behnam <farbod.behnam@gmail.com>
2023-02-13 15:38:23 +0330

    add fetch data on component reload functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useCallback, useEffect, useState } from 'react';
 import './App.css';
 import MoviesList from './components/Movies/MovieList/MovieList';
 import { MovieSwapiData } from './models/movie-swapi-data.model';
@@ -8,41 +8,65 @@ export default function App() {
 
   const [movies, setMovies] = useState<MovieModel[]>([])
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
 
     setIsLoading(true);
+    let transformedMovies: MovieModel[] = []
 
-    const response = await fetch("https://swapi.dev/api/films/")
-    const data = await response.json();
+    try {
+      const response = await fetch("https://swapi.dev/api/films/")
 
-    const transformedMovies: MovieModel[] = data.results.map((movieData: MovieSwapiData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date
-      };
-    });
+      if (response.ok === false) {
+        throw new Error("Something went wrong!");
+      }
+
+      const data = await response.json();
+
+
+
+      transformedMovies = data.results.map((movieData: MovieSwapiData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        };
+      });
+    } catch (error) {
+      const er = error as Error;
+      setError(er.message);
+      setIsLoading(false);
+    }
+
 
     setMovies(transformedMovies);
 
     setIsLoading(false);
 
-  };
+  }, []);
+
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
 
 
-return (
-  <Fragment>
-    <section>
-      <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-    </section>
-    <section>
-      {(isLoading === false && movies.length > 0 )&& <MoviesList movies={movies} />}
-      {(isLoading === false && movies.length === 0) && <p>Found no movies!</p>}
-      {isLoading === true && <p>Loading ...</p>}
-    </section>
-  </Fragment>
-);
+
+
+  return (
+    <Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && !error && <p>Found no movies!</p>}
+        {isLoading && <p>Loading ...</p>}
+        {!isLoading && error && <p>{error}</p>}
+      </section>
+    </Fragment>
+  );
 }

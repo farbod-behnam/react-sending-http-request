7212516e31954ad4f79c5dee79e5171dff01de21
farbod-behnam <farbod.behnam@gmail.com>
2023-02-13 10:53:08 +0330

    implement async await

--- src/App.tsx
+++ src/App.tsx
@@ -8,33 +8,34 @@ export default function App() {
 
   const [movies, setMovies] = useState<MovieModel[]>([])
 
-  const fetchMoviesHandler = () => {
-    fetch("https://swapi.dev/api/films/")
-      .then(response => {
-        return response.json();
-      }).then(data => {
-        const transformedMovies: MovieModel[] = data.results.map((movieData: MovieSwapiData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date
-          };
-        })
-        setMovies(transformedMovies);
-      });
+  async function fetchMoviesHandler() {
+
+    const response = await fetch("https://swapi.dev/api/films/")
+    const data = await response.json();
+
+    const transformedMovies: MovieModel[] = data.results.map((movieData: MovieSwapiData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date
+      };
+    });
+
+    setMovies(transformedMovies);
+
   };
 
 
 
-  return (
-    <Fragment>
-      <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-      </section>
-      <section>
-        <MoviesList movies={movies} />
-      </section>
-    </Fragment>
-  );
+return (
+  <Fragment>
+    <section>
+      <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+    </section>
+    <section>
+      <MoviesList movies={movies} />
+    </section>
+  </Fragment>
+);
 }
